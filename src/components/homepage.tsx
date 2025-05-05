f321a3def0aa596d93271848e473b44f0efdef72
Navneet2605 <navneetpcs3771@gmail.com>
2025-05-06 02:18:07 +0530

    added the home page forms to calculate the EMI

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -1,10 +1,56 @@
+import Button from '@mui/material/Button'
+import { Box, Paper, TextField, Typography, Stack } from '@mui/material';
 import React from 'react'
-import Navbar from './navbar'
 
 const Homepage = () => {
   return (
   <>
-<h1>Hello test</h1>
+<Box
+      sx={{
+        height: '20vh', // Full viewport height
+        display: 'flex',
+        justifyContent: 'left',
+        alignItems: 'center',
+        paddingLeft: '12%'
+        
+      }}
+    >
+      <Paper elevation={0} sx={{ p: 4 }}>
+        <Typography variant="h4" align="left" gutterBottom>
+          Loan Calculator Dashboard
+        </Typography>
+        <Stack direction="row" spacing={2}>
+          <TextField
+            required
+            id="loan-amount"
+            label="Loan Amount"
+            variant="outlined"
+            defaultValue="1000"
+          />
+          <TextField
+            required
+            id="interest-rate"
+            label="Interest Rate (%)"
+            variant="outlined"
+            defaultValue="7.5"
+          />
+          <TextField
+            required
+            id="term-years"
+            label="Term (Years)"
+            variant="outlined"
+            defaultValue="2"
+          />
+        </Stack>
+        <Stack sx={{
+            paddingTop:"2%"
+        }}
+        direction="row"
+        >
+        <Button variant="contained">Calculate</Button>
+        </Stack>
+      </Paper>
+    </Box>
   </>
   )
 }
